7ab0f3b228ec15c69a31e4f8b9186e98fcac25a2
Dollium <laurali@outlook.com>
2017-04-24 21:53:44 +0300

    Update schoolToDTB.php

--- schoolToDTB.php
+++ schoolToDTB.php
@@ -9,31 +9,39 @@ include 'config.php';
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 session_start();
-$ID = $_SESSION['schoolID'];
-if(isset($_POST['nameManually'])&& $_POST['surNameManually'] && $_POST['emailManually']){
+$ID = mysqli_real_escape_string($conn, ($_POST['schoolID']));
+
+if(isset($_POST['name'])&& $_POST['surname'] && $_POST['email']){
     var_dump($_POST);
-    echo "School Id is ".$_SESSION['schoolID'];
+    echo "School Id is ".$ID;
     $insertToUser = mysqli_query($conn, "INSERT INTO user
-    (Email,First_name, Last_name, Password, user_type_id ) VALUES ('".$_POST['emailManually']."','".$_POST['nameManually']."','".$_POST['surNameManually']."','".md5(123456)."'
+    (Email,First_name, Last_name, Password, user_type_id ) VALUES ('".$_POST['email']."','".$_POST['name']."','".$_POST['surname']."','".md5(123456)."'
 , 1)");
+
     if(!$insertToUser){
-        echo ("<SCRIPT LANGUAGE='JavaScript'>
-        window.alert(\"Some one have taken this email!!!!\")
-        window.location.href='addstudent.php'
-        </SCRIPT>");
+      $_SESSION['email_taken'] = 'This email is already in the database.';
     }
 
-    $query = "SELECT user_id FROM user WHERE Email = '".$_POST['emailManually']."'";
-    $result = mysqli_query($conn, $query);
-    while($row = mysqli_fetch_array($result))
-    {
-        $thisSchoolID = $row[0];
+    else {
+      $query = "SELECT user_id FROM user WHERE Email = '".$_POST['email']."'";
+      $result = mysqli_query($conn, $query);
+      while($row = mysqli_fetch_array($result))
+      {
+          $thisSchoolID = $row[0];
+
+      }
+   echo $thisSchoolID.$ID;
+
+      $insertToSchool = mysqli_query($conn, "UPDATE `school` SET
+      `school_admin_id` = '$thisSchoolID' WHERE `school_id` = '$ID'");
+      echo  $insertToSchool;
+      $_SESSION['success_registration'] = 'The user has successfully been registered';
+
+        }
 
     }
- echo $thisSchoolID.$_SESSION['schoolID'];
 
-    $insertToSchool = mysqli_query($conn, "UPDATE `school` SET
-    `school_admin_id` = '$thisSchoolID' WHERE `school_id` = '$ID'");
-    echo  $insertToSchool;
-
+else {
+      $_SESSION['error'] = 'There was an error with your input. Please make sure all fields are filled out.';
 }
+header('location:addSclAd.php');
