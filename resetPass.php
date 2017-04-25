b3437bae914b8a7bfa882131b458462e589a9610
Dollium <laurali@outlook.com>
2017-04-26 00:17:41 +0300

    Add files via upload

---
+++ resetPass.php
@@ -0,0 +1,31 @@
+<?php
+include 'config.php';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
+
+if(isset($_POST['email_result'])) {
+  echo $_POST['email_result'];
+  $email = mysqli_real_escape_string($conn, ($_POST['email_result']));
+
+  if (mysqli_query($conn, "SELECT user WHERE Email='$email'"))
+  {
+  if (mysqli_query($conn, "UPDATE user SET Password= md5(123456) WHERE Email='$email'"))
+  {
+      echo "You have successfully changed user ". $email ."'s password.";
+      $_SESSION['pass_reset'] = "You have successfully changed user ". $email ."'s password.";
+  }
+}
+  else
+  {
+      $_SESSION['email_exist'] = "Email doesn't exist";
+      mysqli_error($conn);
+  }
+}
+
+else {
+  $_SESSION['error'] = 'There was an error with your input. Please make sure all fields are filled out.';
+
+}
+header('Location: ' . $_SERVER['HTTP_REFERER']);
---
+++ search.php
@@ -0,0 +1,29 @@
+<?php
+include 'config.php';
+session_start();
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+// Escape user inputs for security
+$term = mysqli_real_escape_string($conn, $_REQUEST['term']);
+
+if(isset($term)){
+    // Attempt select query execution
+    $sql = "SELECT * FROM user WHERE Email LIKE '" . $term . "%'";
+    if($result = mysqli_query($conn, $sql)){
+        if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_array($result)){
+                echo "<p>" . $row['Email'] . "</p>";
+            }
+            // Close result set
+            mysqli_free_result($result);
+        } else{
+            echo "<p>No matches found</p>";
+        }
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
+}
+
+// close connection
+mysqli_close($conn);
+?>
