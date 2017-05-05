26d712223a54cf67ca860ad91a76dc7be718a280
Dollium <laurali@outlook.com>
2017-05-05 18:23:24 +0300

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -5,31 +5,33 @@ error_reporting(E_ALL);
 session_start();
 
 
-if(isset($_GET['did'])) {
-    $delete_id = mysqli_real_escape_string($conn, $_GET['did']);
-    $sql = mysqli_multi_query($conn, "DELETE FROM user WHERE user_id = '".$delete_id."'");
+if(isset($_GET['did']))
+{
+  $delete_id = mysqli_real_escape_string($conn, $_GET['did']);
+  $sql = mysqli_multi_query($conn, "DELETE FROM user WHERE user_id = '".$delete_id."'");
+
     if($sql)
     {
+      $delete_id = mysqli_real_escape_string($conn, $_GET['did']);
+      $sql2 = mysqli_query($conn, "DELETE FROM student WHERE user_id = '".$delete_id."'");
 
+      if($sql2)
+      {
         $delete_id = mysqli_real_escape_string($conn, $_GET['did']);
-        $sql2 = mysqli_query($conn, "DELETE FROM student WHERE user_id = '".$delete_id."'");
+        $sql3 = mysqli_query($conn, "DELETE FROM student_visits WHERE user_id = '".$delete_id."'");
 
-
-        if($sql2)
+        if($sql3)
         {
-          $delete_id = mysqli_real_escape_string($conn, $_GET['did']);
-          $sql3 = mysqli_query($conn, "DELETE FROM student_visits WHERE user_id = '".$delete_id."'");
-          echo "<br/><br/><span>deleted successfully...!!</span>";
-
+          $_SESSION['delete_success'] = "Käyttäjä poistettu onnistuneesti";
         }
-        else {
-            echo "no, HERE";
-          }
       }
-      else {
-          echo "ERROR HERE";
-        }
     }
+    else
+    {
+      $_SESSION['delete_unsuccess'] = "Poisto epäonnistui.";
+    }
+  }
+
 
     // Deletion through checkboxes
 elseif($_POST["dltBox"]) {
@@ -48,24 +50,17 @@ elseif($_POST["dltBox"]) {
         echo "Deletion successful!";
         if($sql)
         {
-
-
             $sql = mysqli_query($conn, "DELETE FROM student WHERE user_id IN ($delSql)");
-
-
             if($sql2)
             {
 
               $sql = mysqli_query($conn, "DELETE FROM student_visits WHERE user_id IN ($delSql)");
-              echo "<br/><br/><span>deleted successfully...!!</span>";
+              $_SESSION['delete_success'] = "Käyttäjä poistettu onnistuneesti";
 
             }
-            else {
-                echo "no, HERE";
-              }
           }
           else {
-              echo "ERROR HERE";
+              $_SESSION['delete_unsuccess'] = "Poisto epäonnistui.";
             }
       }
 }
