7dec8ab62d5ed14f06d2fe8a80340e274383fa94
Dollium <laurali@outlook.com>
2017-05-07 15:03:54 +0300

    Update studentVisitLog.php

--- studentVisitLog.php
+++ studentVisitLog.php
@@ -21,6 +21,34 @@ $ID = $_SESSION['id'];
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
     <title>Title</title>
+    <style>
+    .prev
+    {
+      margin-right: 50px;
+      text-decoration:underline;
+
+    }
+    .next
+    {
+    margin-left: 50px;
+    text-decoration:underline;
+    }
+    .pag
+    {
+      text-align: center;
+    }
+    @media screen and (max-width: 640px) {
+      .prev
+      {
+        margin-right: 30px;
+
+      }
+      .next
+      {
+        margin-left: 30px;
+      }
+    }
+    </style>
 </head>
 
 <body>
@@ -52,7 +80,7 @@ $ID = $_SESSION['id'];
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#projects">
-                                <i class="fa fa-cog" aria-hidden="true"></i> Setting
+                                <i class="fa fa-cog" aria-hidden="true"></i>Setting
                             </a>
                         </li>
                     </ul>
@@ -65,34 +93,48 @@ $ID = $_SESSION['id'];
         <!-- /NAV -->
 <br/>
         <!-- INSTITUTION  LIST-->
-        <form name="inst" class="form-inline" method="post">
+        <form name="inst" class="form-inline" method="post" action="studentVisitLog.php">
             <div class="text-sm-center">
-              <select class="custom-select mb-2 mr-sm-2 mb-sm-0 institution" name="institution" id="institution">
-                  <?php
-                  $query = "SELECT * FROM institution";
-                  $result = mysqli_query($conn, $query);
-                  while($row = mysqli_fetch_array($result))
-                  {
-                     echo  "<option value=". $row["institution_id"] ." selected>".$row["Name"]."</option>";
-                  }
-                  ?>
+              <select class="custom-select mb-2 mr-sm-2 mb-sm-0 institution" name="institution" id="institution" onchange="this.form.submit();">
 
-            </select>
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                      <?php
+                      $query = "SELECT Name, institution_id FROM institution";
+                      $result = mysqli_query($conn, $query);
+                      echo "<option name='institution' value=''> Kaikki </option>";
+                      while($row = mysqli_fetch_array($result))
+                      {
+                         $selected = (isset($_POST['institution']) && $_POST['institution']  == $row['institution_id']) ? ' selected="selected"' : '';
+                         echo  "<option name='institution' value=". $row["institution_id"] ." ". $selected .">".$row["Name"]."</option>";
+                      }
+
+                      ?>
+
+              </select>
               </div>
 
         </form>
+        <?php
+        if(isset($_POST['institution']) && !empty($_POST['institution']))
+        {
+          $_SESSION['inst_filt'] = $_POST['institution'];
+        }
+        if(isset($_POST['institution']) && empty($_POST['institution']))
+        {
+            $_SESSION['inst_filt'] = '';
+        }
+
+        $inst_filt = $_SESSION['inst_filt'];
+        echo $inst_filt;
+
+
+        ?>
 <br/>
 
-        <form  method="POST">
-            <button name="scan" type="submit" class="btn btn-primary scan">Scan QR</button>
-        </form>
-        <br/>
         <?php
 
         if (isset($_POST['institution'])){
             $_SESSION['inst'] = mysqli_real_escape_string($conn, ($_POST['institution']));
-$inst = $_SESSION['inst'];
+            $inst = $_SESSION['inst'];
 
         }
 
@@ -107,18 +149,67 @@ $inst = $_SESSION['inst'];
         <div class="logTable">
             <table>
               <?php
+              //check if the starting row variable was passed in the URL or not
+              if (!isset($_GET['p']) or !is_numeric($_GET['p'])) {
+                //we give the value of the starting row to 0 because nothing was found in URL
+                $p = 1;
+              //otherwise we take the value from the URL
+              } else {
+                $p = (int)$_GET['p'];
+              }
 
-              $sql = "SELECT vi.time as visit_time, ins.Name as name, vi.user_id as id FROM user as us LEFT JOIN student_visits AS vi ON vi.user_id = us.user_id LEFT JOIN institution AS ins ON ins.institution_id = vi.institution_id WHERE vi.user_id = '$ID' ORDER BY visit_time DESC LIMIT 10";
+                if(isset($_POST['institution']) && !empty($_POST['institution']))
+                {
+
+                $sql = "SELECT vi.time as visit_time, ins.Name as name, vi.user_id as id
+                FROM user as us
+                LEFT JOIN student_visits AS vi ON vi.user_id = us.user_id
+                LEFT JOIN institution AS ins ON ins.institution_id = vi.institution_id
+                WHERE vi.user_id = '$ID'
+                AND vi.institution_id ='$inst_filt'
+                ORDER BY visit_time DESC";
+
+              }
+              else {
+                $sql = "SELECT vi.time as visit_time, ins.Name as name, vi.user_id as id
+                FROM user as us
+                LEFT JOIN student_visits AS vi ON vi.user_id = us.user_id
+                LEFT JOIN institution AS ins ON ins.institution_id = vi.institution_id
+                WHERE vi.user_id = '$ID'
+                ORDER BY visit_time
+                DESC LIMIT $p, 15";
+            }
+
 
               $result = $conn->query($sql);
 
               while($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td>" . $row["name"]. " </td><td> " . $row["id"]. " </td><td> " . $row["visit_time"]. "</td></tr>";
-            } ?>
+            echo "<tr><td>" . $row["name"]. " </td><td> " . date('j.n.Y', strtotime($row["visit_time"])) . "</td></tr>";
+          } ?>
+
             </table>
+          </br></br>
+
+
+
         </div>
-
-
+        <div class="logTable pag">
+        <?php
+          //now this is the link..
+          $prev = $p - 1;
+          if($_SESSION['inst_filt'] == '')
+          {
+          //only print a "Previous" link if a "Next" was clicked
+          if ($prev >= 1) {
+              echo '<a class="previous" href="'.$_SERVER['PHP_SELF'].'?p='.$prev.'">Previous</a>';
+            }
+            if ($prev < 1) {
+                echo '<a class="previous" href="" style="color:#b7b7b7;text-decoration: none;">Previous</a>';
+              }
+            echo '<a class="next" href="'.$_SERVER['PHP_SELF'].'?p='.($p+1).'">Next</a>';
+          }
+          ?>
+        </div>
     </div>
 
 
@@ -127,6 +218,8 @@ $inst = $_SESSION['inst'];
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
 </body>
 
 </html>
