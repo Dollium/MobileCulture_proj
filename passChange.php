fb1d4e34d79b6739b583a106789c59515a5ed3d7
Dollium <laurali@outlook.com>
2017-05-09 17:22:07 +0300

    Update passChange.php

--- passChange.php
+++ passChange.php
@@ -1,47 +1,48 @@
 <?php
 session_start();
 include 'config.php';
+
 $passwordCheck = mysqli_real_escape_string($conn, md5($_POST['oldPassword']));
 $password1 = mysqli_real_escape_string($conn, md5($_POST['newPassword']));
 $password2 = mysqli_real_escape_string($conn, md5($_POST['confirmPassword']));
 $username = mysqli_real_escape_string($conn, $_SESSION['username']);
-print_r($_SESSION);
-echo strlen($_POST['newPassword']);
-$uppercase = preg_match('@[A-Z]@', $password);
-$lowercase = preg_match('@[a-z]@', $password);
-$number    = preg_match('@[0-9]@', $password);
+$re = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/";
+ print_r($_SESSION);
+$pattern = preg_match($re, $_POST['newPassword']);
 
 if(isset($_POST['submit']))
 {
-$error = 0;
+
   if ($passwordCheck != $_SESSION['password'])
     {
       $_SESSION['Oldpass'] = "Virheellinen salasana";
-      $error = 1;
+
     }
 
   if ($password1 <> $password2)
   {
 
    $_SESSION['passMatch'] = "Salasanasi eivät vastaa toisiaan";
-   $error = 1;
+
 
   }
-  if (!$uppercase || !$lowercase || !$number) {
-
-    $_SESSION['pass_str'] = "Vähintään 1 iso kirjain, 1 pieni kirjain sekä 1 numero vaadittu.";
-    $error = 1;
+  if (!$pattern)
+  {
+  $_SESSION['pass_str'] = "Vähintään 1 iso kirjain, 1 pieni kirjain sekä 1 numero vaadittu.";
 
   }
-  if($error = 1)
+echo $error;
+  if(isset($_SESSION['pass_str']) && !empty($_SESSION['pass_str']) || isset($_SESSION['passMatch']) && !empty($_SESSION['passMatch']) || isset($_SESSION['Oldpass']) && !empty($_SESSION['Oldpass']))
   {
     if(($_SESSION['isAdmin'] == true) || ($_SESSION['isSchool'] == true) || ($_SESSION['isStudent']== true))
   {
+
     header('location:settings.php');
     exit;
   }
-  else {
-    header('location:newPassword.php');
+  else{
+    $_SESSION['bbbb'] = 'bbbb';
+    print $error;
     exit;
   }
 }
